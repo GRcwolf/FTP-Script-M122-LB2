e3098331aa18281d9cf14abb070d53621f39a06a
Chris Wolf <39116095+GRcwolf@users.noreply.github.com>
2020-03-24 16:17:33 +0100

    Sufficed class names with class type

--- src/Command/ProcessInvoicesCommand.php
+++ src/Command/ProcessInvoicesCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ProcessInvoices extends Command
+class ProcessInvoicesCommand extends Command
 {
     protected static $defaultName = 'app:invoices:process';
 
