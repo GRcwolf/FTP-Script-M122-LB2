0cfcdb6fbdee1dab08f393272754d32bbad319d4
Chris Wolf <39116095+GRcwolf@users.noreply.github.com>
2020-03-24 15:10:50 +0100

    Added job get command

--- src/Command/GetJobFiles.php
+++ src/Command/GetJobFiles.php
@@ -4,7 +4,44 @@
 namespace App\Command;
 
 
-class GetJobFiles
-{
+use App\Service\ImportJobsFtp;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
+class GetJobFiles extends Command
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected static $defaultName = 'app:get-jobs';
+
+    /**
+     * \App\Service\ImportJobsFtp object.
+     *
+     * @var ImportJobsFtp
+     */
+    private $jobImporter;
+
+    public function __construct(ImportJobsFtp $jobImporter)
+    {
+        $this->jobImporter = $jobImporter;
+
+        // Parent constructor call.
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        // Set the command description.
+        $this->setDescription('Imports invoice jobs.');
+        // Set the help text for the command.
+        $this->setHelp('Imports the invoice jobs from the configured ftp server');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->jobImporter->getJobs();
+        return 0;
+    }
 }
--- src/Service/ImportJobsFtp.php
+++ src/Service/ImportJobsFtp.php
@@ -4,7 +4,156 @@
 namespace App\Service;
 
 
+use Ijanki\Bundle\FtpBundle\Exception\FtpException;
+use Ijanki\Bundle\FtpBundle\Ftp;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 class ImportJobsFtp
 {
+    /**
+     * The FTP-host.
+     *
+     * @var string
+     *  IP or hostname of the ftp-server.
+     */
+    private $ftpHost = '';
 
+    /**
+     * The FTP-user.
+     *
+     * @var string
+     */
+    private $ftpUser = '';
+
+    /**
+     * The FTP-password.
+     *
+     * @var string
+     */
+    private $ftpPassword = '';
+
+    /**
+     * The FTP-client.
+     *
+     * @var Ftp
+     */
+    protected $ftpClient;
+
+    /**
+     * \Psr\Log\LoggerInterface definition.
+     *
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * The Directory containing the invoice jobs.
+     *
+     * @var string
+     */
+    private $ftpDirectory = '';
+
+    /**
+     * The folder to store jobs.
+     *
+     * @var string
+     */
+    private $jobDir = '';
+
+    /**
+     * ImportJobsFtp constructor.
+     *
+     * @param LoggerInterface $logger
+     * @param Ftp $ftp
+     *
+     * @throws FtpException
+     */
+    public function __construct(LoggerInterface $logger, Ftp $ftp)
+    {
+        $this->ftpClient = $ftp;
+        $this->logger = $logger;
+        // Ftp settings.
+        $this->ftpHost = $_ENV['FTP_HOST'];
+        $this->ftpUser = $_ENV['FTP_USER'];
+        $this->ftpPassword = $_ENV['FTP_PASSWORD'];
+        $this->ftpDirectory = $_ENV['FTP_SCHOOLER_OUT'];
+
+        $this->jobDir = $_ENV['JOB_DIR'];
+
+        // Configure the FTP client.
+        $this->configureFtpConnection();
+        $this->getJobs();
+    }
+
+    /**
+     * Handle object destruction.
+     */
+    public function __destruct()
+    {
+        // Close the ftp connection.
+        $this->ftpClient->close();
+    }
+
+    /**
+     * Sets up the ftp-client with the configuration.
+     *
+     * @throws FtpException
+     */
+    protected function configureFtpConnection()
+    {
+        try {
+            $this->ftpClient->connect($this->ftpHost);
+            $this->ftpClient->login($this->ftpUser, $this->ftpPassword);
+        } catch (FtpException $exception) {
+            $this->logger->error('Could not connect to ' . $this->ftpHost . ' with the provided credentials. Error: ' . $exception->getMessage());
+            throw $exception;
+        }
+    }
+
+    /**
+     * Gets all job files and downloads the locally.
+     */
+    public function getJobs()
+    {
+        $jobFiles = $this->getJobFileNames();
+        foreach ($jobFiles as $jobFile) {
+            $this->getJobFile($jobFile);
+        }
+    }
+
+    /**
+     * Get the files which will be processed.
+     *
+     * @return array
+     */
+    private function getJobFileNames()
+    {
+        $filePattern = '/\/?\w+\d+\.data$/';
+
+        $ftpFiles = $this->ftpClient->nlist($this->ftpDirectory);
+        $filesToProcess = [];
+        foreach ($ftpFiles as $file) {
+            if (preg_match($filePattern, $file)) {
+                $filesToProcess[] = $file;
+            }
+        }
+        return $filesToProcess;
+    }
+
+    /**
+     * Downloads a job file.
+     *
+     * @param string $file
+     *  The job file.
+     */
+    private function getJobFile(string $file)
+    {
+        // Set file locations.
+        $remoteFile = $file;
+        $localFile = $this->jobDir . '/' . $file;
+
+        // Download file.
+        $this->ftpClient->get($localFile, $remoteFile, FTP_BINARY);
+    }
 }
