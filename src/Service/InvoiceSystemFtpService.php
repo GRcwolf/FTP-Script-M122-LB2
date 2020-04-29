df901039e41a9291b5ed0c3cca1ef6ce11a187dd
Chris Wolf <39116095+GRcwolf@users.noreply.github.com>
2020-04-29 20:51:34 +0200

    Added methods to download receipts

--- src/Service/InvoiceSystemFtpService.php
+++ src/Service/InvoiceSystemFtpService.php
@@ -7,6 +7,7 @@ namespace App\Service;
 use App\Exception\FtpConnectionFailedException;
 use Exception;
 use Ijanki\Bundle\FtpBundle\Ftp;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Finder\SplFileInfo;
 
@@ -48,20 +49,27 @@ class InvoiceSystemFtpService
   private $cleanUpService;
 
   /**
+   * @var Filesystem
+   */
+  private $filesystem;
+
+  /**
    * InvoiceSystemFtpService constructor.
    *
    * @param LoggerService $logger
    * @param ContainerParametersHelper $helper
    * @param Ftp $ftp
    * @param CleanUpService $cleanUpService
+   * @param Filesystem $filesystem
    */
-  public function __construct(LoggerService $logger, ContainerParametersHelper $helper, Ftp $ftp, CleanUpService $cleanUpService)
+  public function __construct(LoggerService $logger, ContainerParametersHelper $helper, Ftp $ftp, CleanUpService $cleanUpService, Filesystem $filesystem)
   {
     $this->logger = $logger;
     $this->cleanUpService = $cleanUpService;
     $this->finder = new Finder();
     $this->helper = $helper;
     $this->ftp = $ftp;
+    $this->filesystem = $filesystem;
   }
 
   /**
@@ -153,5 +161,64 @@ class InvoiceSystemFtpService
       }
     }
     return $files;
+  }
+
+  /**
+   * Downloads the receipt files and afterwards deletes them on the server.
+   */
+  public function downloadReceipts(): void
+  {
+    try {
+      $this->initializeConnection();
+    } catch (FtpConnectionFailedException $exception) {
+      // Abort if connection could not be established.
+      return;
+    }
+    // Navigate to output directory of ftp server.
+    $this->ftp->chdir('/' . $_ENV['FTP_INVOICE_OUT']);
+    // Create the path to store the receipts.
+    $pathToStore = $this->helper->getTempFilesFolder() . '/receipts';
+    $this->filesystem->mkdir($pathToStore);
+    $ftpFiles = $this->ftp->nlist('.');
+    foreach ($ftpFiles as $ftpFile) {
+      // Check if the current file is a receipt.
+      if (preg_match('/^quittungsfile\d+_\d+\.txt$/', $ftpFile)) {
+        // Download the file.
+        $this->downloadSingleReceipt($ftpFile, $pathToStore);
+      }
+    }
+  }
+
+  /**
+   * Downloads a single receipt to a defined local directory.
+   *
+   * @param string $name
+   * @param string $storageDir
+   */
+  private function downloadSingleReceipt(string $name, string $storageDir): void
+  {
+    try {
+      $localFile = $storageDir . '/' . $name;
+      $this->ftp->get($localFile, $name, FTP_BINARY);
+      $this->logger->info('Downloaded the receipt ' . $name);
+      $this->deleteSingleRemoteReceipt($name);
+    } catch (Exception $exception) {
+      $this->logger->error('An error occurred while downloading the receipt ' . $name . ' Error: ' . $exception->getMessage());
+    }
+  }
+
+  /**
+   * Deletes a single remote receipt.
+   *
+   * @param string $name
+   */
+  private function deleteSingleRemoteReceipt(string $name): void
+  {
+    try {
+      $this->ftp->delete($name);
+      $this->logger->info('The remote receipt ' . $name . ' has been deleted.');
+    } catch (Exception $exception) {
+      $this->logger->error('Could not delete the remote file ' . $name . ', reason: ' . $exception->getMessage());
+    }
   }
 }
